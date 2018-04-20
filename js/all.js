5c616761b5a7a737eb814cf2af5a2c262e3edf42
Melissa Keizer <melissa.keizer@theappbuilder.com>
2018-04-20 15:43:13 +0100

    mobile navigation

---
+++ js/all.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  // Mobile navigation
+  $('#navigation__mobile__icon').on( "click", function( event ) {
+		$('.navigation__mobile').addClass( "visible" );
+  });
+
+});
